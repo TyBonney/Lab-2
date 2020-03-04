e6af7b848fd775b202a123a8df742518b0af29d7
Ty Bonney <c3350755@uon.edu.au>
2020-03-05 10:48:59 +1100

    Initial comment

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main () {
+    float a;
+    float b;
+    float c;
+    float d;
+    float x1;
+    float x2;
+    float n;
+
+    scanf("%f %f %f", &a, &b, &c);
+    d = b*b - 4*a*c;
+
+    if (d<0); {
+        n==0;
+}
+    if else (d==0); {
+        n=1;
+        x1=(-b/(2=a));
+}
+    if else (d>0); {
+        n=2;
+        x1=((-b)+sqrt(d))/(2*a);
+        x2=((-b)+sqrt(d))/(2*a);
+}
+    printf("%f\n", x);
+
+    return 0;
+}
+
+
+
